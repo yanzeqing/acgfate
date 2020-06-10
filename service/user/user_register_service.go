03ce3f9d3c65815c9977176b005b4248b3dbe482
Secriy <secriyal@gmail.com>
2020-06-10 15:52:27 +0800

    修改参数

--- service/user/user_register_service.go
+++ service/user/user_register_service.go
@@ -7,29 +7,20 @@ import (
 
 // RegisterService 管理用户注册服务
 type RegisterService struct {
-	UserName        string `form:"user_name" json:"user_name" binding:"required,ascii,min=2,max=30"`
-	Nickname        string `form:"nickname" json:"nickname" binding:"required,min=2,max=30"`
-	Password        string `form:"password" json:"password" binding:"required,min=8,max=40"`
-	PasswordConfirm string `form:"password_confirm" json:"password_confirm" binding:"required,eqfield=Password,min=8,max=40"`
+	UserName        string `form:"user_name" json:"user_name" binding:"required,ascii,min=3,max=10"`
+	Nickname        string `form:"nickname" json:"nickname" binding:"required,min=1,max=10"`
+	Password        string `form:"password" json:"password" binding:"required,min=8,max=16"`
+	PasswordConfirm string `form:"password_confirm" json:"password_confirm" binding:"required,eqfield=Password,min=8,max=16"`
 }
 
 // valid 验证表单
 func (service *RegisterService) valid() *serializer.Response {
 	count := 0
-	model.DB.Model(&model.User{}).Where("nickname = ?", service.Nickname).Count(&count)
-	if count > 0 {
-		return &serializer.Response{
-			Code: 40001,
-			Msg:  "昵称被占用",
-		}
-	}
-
-	count = 0
 	model.DB.Model(&model.User{}).Where("user_name = ?", service.UserName).Count(&count)
 	if count > 0 {
 		return &serializer.Response{
 			Code: 40001,
-			Msg:  "用户名已经注册",
+			Msg:  "用户名已被注册",
 		}
 	}
 
